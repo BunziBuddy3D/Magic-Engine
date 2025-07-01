d3615969b3c89a6174336016eaebc41164706716
BunziBuddy3D <shahabi.kian@gmail.com>
2025-07-01 16:16:10 +0330

    added a color for surface of cube and chnage backgrond color for better Understand and chnage cube function

--- main/main.py
+++ main/main.py
@@ -35,7 +35,37 @@ edges = (
     (5,4),
     (5,7)
     )
+colors = (
+    (1,0,0),
+    (0,1,0),
+    (0,0,1),
+    (0,1,0),
+    (1,1,1),
+    (0,1,1),
+    (1,0,0),
+    (0,1,0),
+    (0,0,1),
+    (1,0,0),
+    (1,1,1),
+    (0,1,1),
+    )
+surfaces = (
+    (0,1,2,3),
+    (3,2,7,6),
+    (6,7,5,4),
+    (4,5,1,0),
+    (1,5,7,2),
+    (4,0,3,6)
+    )
 def Cube():
+    glBegin(GL_QUADS)
+    for surface in surfaces:
+        x = 0
+        for vertex in surface:
+            x+=1
+            glColor3fv(colors[x])
+            glVertex3fv(verticies[vertex])
+    glEnd()
     glBegin(GL_LINES)
     for edge in edges:
         for vertex in edge:
@@ -52,7 +82,7 @@ while running:
  # code of program for nikan of Kian (: 
  #--------------------------------------------------
     glRotatef(1, 3, 1, 1)
-    glClearColor(23/255.0 , 125/255.0 , 144/255.0 , 1.0)
+    glClearColor(0/255.0 , 0/255.0 , 0/255.0 , 1.0)
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
     Cube()
     
